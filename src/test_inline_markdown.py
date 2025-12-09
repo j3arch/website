321d153cdb30b9623ec4efaccf76db0d79e1b257
j3arch <j3archx@gmail.com>
2025-12-09 03:14:14 +0300

    added tests for splits

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,8 @@
 import unittest
 from inline_markdown import (
     split_nodes_delimiter,
+    split_nodes_image,
+    split_nodes_link,
     extract_markdown_links,
     extract_markdown_images,
 )
@@ -78,6 +80,58 @@ class TestInlineMarkdown(unittest.TestCase):
                 ("another link", "https://test.link"),
             ],
             matches,
+        )
+
+    def test_split_images(self):
+        node = TextNode("This is text with an ![image](https://www.example.com/image.png)", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+        self.assertEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://www.example.com/image.png"),
+            ],
+            new_nodes,
+        )
+
+    def test_split_image_single(self):
+        node = TextNode("![image](https://www.example.com/image.png)", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual([TextNode("image", TextType.IMAGE, "https://www.example.com/image.png")], new_nodes)
+
+
+    def test_split_image(self):
+        node = TextNode(
+            "This is text with an ![image](https://www.example.com/image.png) and another ![second image](https://www.example.com/image1.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://www.example.com/image.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://www.example.com/image1.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev) with text that follows",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("another link", TextType.LINK, "https://blog.boot.dev"),
+                TextNode(" with text that follows", TextType.TEXT),
+            ],
+            new_nodes,
         )
 
 
