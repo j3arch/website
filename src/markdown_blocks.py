6556ddf427c24b9939d612700753367e21431d93
j3arch <j3archx@gmail.com>
2025-12-23 23:21:49 +0300

    heading to html node function

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -75,3 +75,16 @@ def paragraph_to_html_node(block):
     paragraph = " ".join(lines)
     children = text_to_children(paragraph)
     return ParentNode("p", children)
+
+def heading_to_html_node(block):
+    level = 0
+    for char in block:
+        if char == "#":
+            level += 1
+        else:
+            break
+    if level + 1 >= len(block):
+        raise ValueError(f"invalid heading level: {level}")
+    text = block[level + 1 :]
+    children = text_to_children(text)
+    return ParentNode(f"h{level}", children)
