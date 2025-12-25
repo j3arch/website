323e40b6bcd63aea297b673b5f8f57a2110664d4
j3arch <j3archx@gmail.com>
2025-12-25 22:44:11 +0300

    ordered list to html node

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -97,3 +97,12 @@ def code_to_html_node(block):
     child = text_node_to_html_node(raw_text_node)
     code = ParentNode("code", [child])
     return ParentNode("pre", [code])
+
+def olist_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[3:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ol", html_items)
