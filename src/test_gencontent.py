bd18ccba4ffe5746ee43dc4025f84178219fe73e
j3arch <j3archx@gmail.com>
2026-01-20 23:15:21 +0300

    gencontent tests

---
+++ src/test_gencontent.py
@@ -0,0 +1,24 @@
+import unittest
+
+from gencontent import extract_title
+
+
+class TestExtractTitle(unittest.TestCase):
+    def test_eq(self):
+        actual = extract_title("# This is a title")
+        self.assertEqual(actual, "This is a title")
+
+    def test_eq_double(self):
+        actual = extract_title(
+            """
+# This is a title
+
+# This is a second title that should be ignored
+"""
+        )
+        self.assertEqual(actual, "This is a title")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
